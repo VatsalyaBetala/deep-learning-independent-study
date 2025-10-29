b01ec28ea06b4dbc6dee3b105119c4230816750c
Vatsalya Betala <150043725+VatsalyaBetala@users.noreply.github.com>
2025-10-30 02:56:00 +0530

    Remove duplicate files in chapter 1 probability

--- ch1_probablity/continuous_distributions/exponential.py
+++
@@ -1,9 +0,0 @@
-import numpy as np
-import matplotlib.pyplot as plt
-
-x = np.linspace(0,5,500)
-for l in [0.5,1,2]:
-    plt.plot(x, l*np.exp(-l*x), label=f'λ={l}')
-plt.xlabel('x'); plt.ylabel('p(x|λ)')
-plt.legend(); plt.title('Exponential Distribution')
-plt.show()
